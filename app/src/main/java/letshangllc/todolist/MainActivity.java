0c1e95f9da27f0800b0c513ce4fb0ea6742fceac
Chase Burnham <cburnhamiv@gmail.com>
2015-11-03 13:16:03 +0100

    initial commit

---
+++ app/src/main/java/letshangllc/todolist/MainActivity.java
@@ -0,0 +1,50 @@
+package letshangllc.todolist;
+
+import android.content.ContentValues;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String PREFS_NAME = "TodoTaskPrefs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+        Set<String> setOfItems = prefs.getStringSet("todoSet", new HashSet<String>());
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_add:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
